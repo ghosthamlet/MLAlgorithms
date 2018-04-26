24ca83c5dc358215de5ade7b313ab3d9b9e7405f
gvvvv <gvvvv@163.com>
2018-04-26 09:05:59 +0800

    change setting

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject mlalgorithms "0.1.0-SNAPSHOT"
   :description "use closure and return fn to capsurize init params or states like class. keyword and multimethod for activiaton loss grad fns. "
   :url "https://github.com/ghosthamlet/MLAlgorithms"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [net.mikera/core.matrix "0.62.0"]])
+  :license  {:name "Eclipse Public License"
+             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies  [[org.clojure/clojure "1.9.0"]
+                  [net.mikera/core.matrix "0.62.0"]])
