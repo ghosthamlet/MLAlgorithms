41e83717547d2928333aae48651a50ac574f51b7
gvvvv <gvvvv@163.com>
2018-04-28 17:48:48 +0800

    clean core.clj

--- src/mlalgorithms/core.clj
+++ src/mlalgorithms/core.clj
@@ -1,8 +1,4 @@
-(ns mlalgorithms.core
-  (:refer-clojure :exclude [* - + / == < <= > >= not= = min max])
-  (:require clojure.core.matrix.impl.ndarray
-            [clojure.core.matrix :refer :all]
-            [clojure.core.matrix.operators :refer :all]))
+(ns mlalgorithms.core)
 
 ;; http://spacemacs.org/layers/+lang/clojure/README.html
 ;; https://github.com/mikera/core.matrix/blob/develop/src/test/clojure/clojure/core/matrix/demo/pagerank.clj
@@ -10,9 +6,3 @@
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
-
-;; interger can be just 64bit, but float no limit
-(dot [[9999990.0 2000000.0 999910000.0] [10.0 9999999920.0 30.0]]
-     [[999999999910000000000999999999910000000000]
-      [9999999999992999999999900099999999999929999999999000]
-      [99999999999999999300009999999999999999930000]])
